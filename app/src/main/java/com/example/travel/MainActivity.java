0ecc7eb04e4889dcbb64fa73e268414f78be2afb
KirillAleshenkov <125723083+KirillAleshenkov@users.noreply.github.com>
2023-03-06 13:41:56 +0500

    Рабочая версия. Было сделано: Отрисована второй активность.

--- app/src/main/java/com/example/travel/MainActivity.java
+++ app/src/main/java/com/example/travel/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.travel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onClick1(View view) {
+        Intent intent = new Intent(MainActivity.this, BigTravel.class);
+        startActivity(intent);
     }
 }
