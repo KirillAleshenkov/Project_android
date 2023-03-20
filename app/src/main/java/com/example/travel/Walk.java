4cbb7f073a33808fc668b490b586f2d97aea4322
KirillAleshenkov <125723083+KirillAleshenkov@users.noreply.github.com>
2023-03-20 21:32:22 +0500

    Рабочая версия. Ошибка по arrayOf, не получилось исправить .

--- app/src/main/java/com/example/travel/Walk.java
+++ app/src/main/java/com/example/travel/Walk.java
@@ -3,6 +3,7 @@ package com.example.travel;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import android.Manifest;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -23,26 +24,18 @@ public class Walk extends AppCompatActivity {
     private MapView map = null;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_walk);
-        /*Configuration.getInstance().load(getAppLicationContext(),PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
-        MapView mapView = new MapView(this);
-        mapView.setTileSource(TileSourceFactory.MAPNIK);
 
-        setContentView(mapView);*/
         Context ctx = getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
+        setContentView(R.layout.activity_walk);
 
         map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
-        requestPermissionsIfNecessary(arrayOf(
-                // if you need to show the current location, uncomment the line below
-                // Manifest.permission.ACCESS_FINE_LOCATION,
-                // WRITE_EXTERNAL_STORAGE is required in order to show the map
-                Manifest.permission.WRITE_EXTERNAL_STORAGE
-        ));
+        requestPermissionsIfNecessary(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE));
     }
     @Override
     public void onResume() {
@@ -56,11 +49,12 @@ public class Walk extends AppCompatActivity {
     }
     @Override
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         ArrayList<String> permissionsToRequest = new ArrayList<>();
         for (int i = 0; i < grantResults.length; i++) {
             permissionsToRequest.add(permissions[i]);
         }
-        if (permissionsToRequest.size > 0) {
+        if (permissionsToRequest.size()> 0) {
             ActivityCompat.requestPermissions(
                     this,
                     permissionsToRequest.toArray(new String[0]),
@@ -76,16 +70,11 @@ public class Walk extends AppCompatActivity {
                 permissionsToRequest.add(permission);
             }
         }
-    }
-    if(permissionsToRequest.size > 0) {
-        ActivityCompat.requestPermissions(
-                this,
-                permissionsToRequest.toArray(new String[0]),
-                REQUEST_PERMISSIONS_REQUEST_CODE);
-    }
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        mMapView = new MapView(inflater.getContext(), 256, getContext());
-        return mMapView;
+        if (permissionsToRequest.size() > 0) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    permissionsToRequest.toArray(new String[0]),
+                    REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
     }
 }
