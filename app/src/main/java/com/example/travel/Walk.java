7e5a007690080ccc59488af0f61bc76b06e30999
KirillAleshenkov <125723083+KirillAleshenkov@users.noreply.github.com>
2023-04-05 23:33:16 +0500

    Рабочая версия. Попытки сделать плавную анимацию.

--- app/src/main/java/com/example/travel/Walk.java
+++ app/src/main/java/com/example/travel/Walk.java
@@ -9,9 +9,12 @@ import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.util.GeoPoint;
@@ -24,7 +27,10 @@ import org.osmdroid.views.overlay.ScaleBarOverlay;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
 import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -34,10 +40,15 @@ public class Walk extends AppCompatActivity {
 
     private final int REQUEST_PERMISSIONS_REQUEST_CODE = 1;
     private MapView mapView = null;
+    private MyLocationNewOverlay myLocationOverlay = null;
+    final Context context = this;
+    //final DisplayMetrics dm = context.getResources().getDisplayMetrics();
+    private CompassOverlay mCompassOverlay;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Configuration.getInstance().load(getApplicationContext(), PreferenceManager.getDefaultSharedPreferences(getApplicationContext()));
@@ -55,10 +66,30 @@ public class Walk extends AppCompatActivity {
         //Поворот карты с помощью жестов
         RotationGestureOverlay rotationGestureOverlay = new RotationGestureOverlay(mapView);
         mapView.getOverlays().add(rotationGestureOverlay);
+        mapView.setMultiTouchControls(true);
         rotationGestureOverlay.setEnabled(true);
+        mapView.setMapOrientation(0);
+        float currentBearing = mapView.getMapOrientation();
+        RotateAnimation RotateAnimation = new RotateAnimation(currentBearing, 0, Animation.RELATIVE_TO_SELF, 0.5f);
+        RotateAnimation.setDuration(1000);
+        mapView.startAnimation(RotateAnimation);
+        //чувтвительность поворота карты
+        //this.mCompassOverlay = new CompassOverlay(context, new InternalCompassOrientationProvider(context), mapView);
+        //this.mCompassOverlay.enableCompass();
+        //mapView.getOverlays().add(this.mCompassOverlay);
+        //Масштаб карты
+        //ScaleBarOverlay mScaleBarOverlay = new ScaleBarOverlay(mapView);
+        //mScaleBarOverlay.setCentred(true);
+        //mScaleBarOverlay.setScaleBarOffset(dm.widthPixels / 2, 10);
+        //mapView.getOverlays().add(this.mCompassOverlay);
+
+
         // установка карты в макет
         setContentView(mapView);
     }
+
+
+
     @Override
     public void onResume() {
         super.onResume();
